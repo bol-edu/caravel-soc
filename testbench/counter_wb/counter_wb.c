70fc2510fe1331af6304e879fe0b3c8e8fab42b0
Kevin Jan <kevin.jan.tw@gmail.com>
2023-03-09 07:34:48 +0800

    Update counter_wb.c

--- testbench/counter_wb/counter_wb.c
+++ testbench/counter_wb/counter_wb.c
@@ -83,7 +83,7 @@ void main()
 	reg_mprj_datal = 0xAB600000;
 
     reg_mprj_slave = 0x00002710;
-    //reg_mprj_datal = 0xAB610000;
+    reg_mprj_datal = 0xAB610000;
     if (reg_mprj_slave == 0x2B3D) {
         reg_mprj_datal = 0xAB610000;
     }
